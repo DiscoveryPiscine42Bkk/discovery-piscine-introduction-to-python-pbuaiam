ea9e1c2979d6bf24cd0db0fadbb7a16001d6553d
pbuaiam <phatita.1520@gmail.com>
2025-06-19 05:34:21 +0000

    greetings_for all.py

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,13 @@
+def greetings(name=None):
+    if name is None:
+        print("Hello, noble stranger.")
+    elif isinstance(name, str):
+        print(f"Hello, {name}.")
+    else:
+        print("Error! It was not a name.")
+
+#ตัวอย่างการเรียกใช้งาน
+greetings('Alexandre')
+greetings('Wil')
+greetings()
+greetings(42)
